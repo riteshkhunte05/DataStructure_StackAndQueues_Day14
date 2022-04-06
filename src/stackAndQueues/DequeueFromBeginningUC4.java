1d0186186a0c17942686cc4046957f50a757b884
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-04-06 16:05:23 +0530

    DeQueue program added

---
+++ src/stackAndQueues/DequeueFromBeginningUC4.java
@@ -0,0 +1,64 @@
+package stackAndQueues;
+
+	public class DequeueFromBeginningUC4 {
+		 public Node head;
+		    public Node tail;
+		    static class Node {
+		        int data;
+		        Node next;
+
+		        Node(int d) {
+		            data = d;
+		            next = null;
+		        }
+		    }
+
+		    // Append datas One By One
+		    public void append(int datas){
+		        Node newNode = new Node(datas);
+		        if(head == null){
+		            head = newNode;
+		            tail = newNode;
+		        }
+		        else {
+		            tail.next = newNode;
+		            tail = newNode;
+		        }
+		    }
+
+		    // Display all the Node with value
+		    public void display() {
+		        Node current = head;
+		        if(head == null) {
+		            System.out.println("List is empty");
+		        }
+		        while (current != null) {
+		            System.out.println(current.data +" ");
+		            current = current.next;
+		        }
+		    }
+
+		    // Dequeue all the elements
+		    public void dequeue() {
+		        Node current = head;
+		        while (current != null) {
+		            if (current == head) {
+		                head = head.next;
+		                current = head;
+		            }
+		        }
+		    }
+		    public static void main(String[] args) {
+		    	DequeueFromBeginningUC4 operQueue = new DequeueFromBeginningUC4();
+		        operQueue.append(70);
+		        operQueue.append(30);
+		        operQueue.append(56);
+
+		        operQueue.display();
+		        operQueue.dequeue();
+		        operQueue.display();
+
+		    }
+		
+	}
+
